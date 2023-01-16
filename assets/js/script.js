5536bf195ff750ad1df7f81a8f93c615a30347fb
unknown <kevwenner@gmail.com>
2023-01-15 21:35:55 -0500

    added card display and iteration for eachday

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var submitBtnEl = document.getElementById("submitBtn");
 var locationInputEl = document.getElementById("exampleDataList");
 var searchHistoryEl = document.getElementById("searchHistory");
+var forcastEl = document.getElementById("forcast");
+var todayWeather= document.getElementById("today");
+var fiveDay = document.getElementById("fiveDay");
 
 
 function getLocation(){
@@ -8,26 +11,26 @@ function getLocation(){
     var locationID = locationInputEl.value;
     console.log(locationID);
     if(locationID){
-        getLocationApi("http://api.openweathermap.org/geo/1.0/direct?q="+locationID+"&limit=1&appid=0cedfaa9454de2de2c5ab1e8aa2d105b");
+        getLocationApi("http://api.openweathermap.org/geo/1.0/direct?q="+locationID+"&limit=1&appid=0cedfaa9454de2de2c5ab1e8aa2d105b", locationID);
     }
     var newLocation = document.createElement("button");
-    var attributes = {class: "btn btn-primary col-12", type: "button", id: locationID}
+    var attributes = {
+        class: "btn btn-primary col-12",
+         type: "button",
+          id: locationID
+        }
 
-    function setAttributes(element, attributes){
-        Object.keys(attributes).forEach(attr => {element.setAttribute(attr, attributes[attr]);
-        });
-    };
-    
+    if(document.getElementById(locationID) == null){
         setAttributes(newLocation, attributes);
         newLocation.textContent += locationID;
         newLocation.style.marginTop = ".5em";
         searchHistoryEl.append(newLocation);
         searchHistoryEl.style.borderTop = "solid";
         locationInputEl.value = "";
-
+    }
 };
 
-function getLocationApi(requestUrl){
+function getLocationApi(requestUrl, location){
     fetch(requestUrl)
         .then(function(response){
             if (response.ok){
@@ -37,7 +40,7 @@ function getLocationApi(requestUrl){
                 console.log(lon);
                 var lat = data[0].lat;
                 console.log(lat);
-                getWetherApi("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=414bc693d7b3bc637f7a83bcddd54bcf");
+                getWetherApi("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=414bc693d7b3bc637f7a83bcddd54bcf", location);
                 });
             }else{
                 alert("Error: " + response.statusText);
@@ -49,13 +52,13 @@ function getLocationApi(requestUrl){
         });            
 };
 
-function getWetherApi(requestUrl){
+function getWetherApi(requestUrl, location){
     fetch(requestUrl)
         .then(function(response){
             if (response.ok){
                  response.json().then(function(data){
                 console.log(data);
-                setUpDisplay(data);
+                setUpDisplay(data, location);
                 });
             }else{
                 alert("Error: " + response.statusText);
@@ -66,11 +69,47 @@ function getWetherApi(requestUrl){
             alert("unable to connect to OpenWeather 5 day weather");
         });            
 }
-
-function setUpDisplay(weatherData){
+function setAttributes(element, attributes){
+        Object.keys(attributes).forEach(attr => {
+            element.setAttribute(attr, attributes[attr]);
+        });
+    };
+function setUpDisplay(weatherData, location){
     console.log(weatherData.list[0]);
-    // creat cards
-    
-}
+    // Today
+    var weatherWeek = [
+        weatherData.list[0], weatherData.list[1], weatherData.list[2], weatherData.list[3], weatherData.list[4], weatherData.list[5]
+    ]
 
+    for (let i = 0; i < weatherWeek.length; i++) {//each index is 3 hours adjust for 24 hours
+        var todaysDate = weatherWeek[i].dt_txt;
+        // console.log(todaysDate);
+        var todaysTemp = weatherWeek[i].main.temp;
+        var todaysWind = weatherWeek[i].wind.speed;
+        var todaysHumidity = weatherWeek[i].main.humidity;
+        var tt = document.getElementById(i+"Temp");
+        var tW = document.getElementById(i+"Wind");
+        var th = document.getElementById(i+"Humidity");
+
+        if (i==0) {
+            var todayEl = document.getElementById("location");
+            todayEl.textContent = location + " " + todaysDate;
+        }else if(i>0){
+            var dayEl = document.getElementById("day"+i);
+            dayEl.textContent = todaysDate;
+        }
+        // var icon = weatherData.list[i].weather[0].icon; 
+        tt.textContent = "Weather: " + tempConvert(todaysTemp); //add trim
+        tW.textContent = "Wind: " + todaysWind + "mph";
+        th.textContent = "Humidity: " + todaysHumidity + "%";
+      
+    }
+};
+
+function tempConvert(temp){
+    var tempF = (1.8 * (temp-273)+32);
+    return tempF
+}
+// make listener event for search history
+// add color swap for active location display
 submitBtnEl.addEventListener("click", getLocation);
